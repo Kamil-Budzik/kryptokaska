38456a732b8e9fb045a057916dc713dd6c7e87a5
ssoltys <htfhere@gmail.com>
2023-09-30 16:27:47 +0200

    feature/1st version of calculating the mean

---
+++ src/utils/weightedMean/ExchangeData.ts
@@ -0,0 +1,37 @@
+export class ExchangeData {
+    private readonly cryptocurrency: string;
+    private readonly volume24h: number;
+    private readonly averageValue24h: number;
+
+    constructor(cryptocurrency: string, volume24h: number, averageValue24h: number) {
+        this.cryptocurrency = cryptocurrency;
+        this.volume24h = volume24h;
+        this.averageValue24h = averageValue24h;
+    }
+
+    // Getters
+    getCryptocurrency(): string {
+        return this.cryptocurrency;
+    }
+
+    getVolume24h(): number {
+        return this.volume24h;
+    }
+
+    getAverageValue24h(): number {
+        return this.averageValue24h;
+    }
+
+    describe(): string {
+        return `The cryptocurrency ${this.cryptocurrency} has a volume of ${this.volume24h} and an average value of ${this.averageValue24h} over the last 24 hours.`;
+    }
+}
+
+// Example usage:
+const bitcoinData = new ExchangeData(
+    'Bitcoin',
+    1000, // example volume in the last 24 hours
+    50000 // example average value in the last 24 hours
+);
+
+console.log(bitcoinData.describe());
