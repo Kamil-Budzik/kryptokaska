9e66afc758b70c5d59907c8354144c7992094122
Kamil Budzik <kbudzik@tbscg.com>
2023-10-01 00:27:57 +0200

    [FE] Manual page milestone 1

--- src/pages/Manual.tsx
+++ src/pages/Manual.tsx
@@ -1,12 +1,92 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import TextField from '@mui/material/TextField';
+import {
+  Button,
+  InputLabel,
+  MenuItem,
+  Select,
+  FormControl,
+  Typography,
+} from '@mui/material';
+import styled from '@emotion/styled';
+
+const REQUIRED_FIELD_MSG = 'To pole jest wymagane';
+
+const schema = yup
+  .object({
+    url: yup.string().url().required(REQUIRED_FIELD_MSG),
+    stockMarketName: yup
+      .number()
+      .positive()
+      .integer()
+      .required(REQUIRED_FIELD_MSG),
+    cryptocurrencyRate: yup.number().positive().required(REQUIRED_FIELD_MSG),
+    currency: yup.string().required(REQUIRED_FIELD_MSG),
+  })
+  .required();
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 function Manual() {
-    return (
-        <>
-            <header>
-                <h1>Manual</h1>
-            </header>
-        </>
-    )
-};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <StyledForm onSubmit={handleSubmit(onSubmit)}>
+      <FormControl fullWidth>
+        <TextField {...register('url')} label="Adres URL giełdy/kantoru" />
+        <Typography>{errors.url?.message}</Typography>
+      </FormControl>
+
+      <FormControl fullWidth>
+        <TextField
+          {...register('stockMarketName')}
+          label="Nazwa giełdy/kantoru"
+        />
+      </FormControl>
+      <p>{errors.stockMarketName?.message}</p>
+
+      <FormControl fullWidth>
+        <TextField
+          {...register('cryptocurrencyRate')}
+          label="Kurs Kryptowaluty"
+        />
+      </FormControl>
+      <p>{errors.cryptocurrencyRate?.message}</p>
+
+      <FormControl fullWidth>
+        <InputLabel id="currency">Waluta</InputLabel>
+        <Select
+          labelId="currency"
+          id="currency"
+          {...register('currency')}
+          label="Waluta"
+          defaultValue="PLN"
+        >
+          <MenuItem value="PLN" defaultChecked>
+            PLN
+          </MenuItem>
+          <MenuItem value="USD">USD</MenuItem>
+        </Select>
+      </FormControl>
+
+      <Button variant="contained" color="primary">
+        Zatwierdź
+      </Button>
+    </StyledForm>
+  );
+}
 
 export default Manual;
--- src/pages/NewReport.tsx
+++ src/pages/NewReport.tsx
@@ -1,127 +1,161 @@
-import {useForm, SubmitHandler, useFieldArray} from "react-hook-form"
-import {Button, Divider, Typography} from "@mui/material";
+import { useForm, SubmitHandler, useFieldArray } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { Button, Divider, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Wrapper from '../components/UI/Wrapper';
 
-
 type Inputs = {
-    enforcementAuthority: string
-    caseNumber: string
-    cryptoCurrencyOwnerData: string
-    cryptoAssets: { cryptoAsset: string; amountOfCryptoAsset: string }[];
-}
+  enforcementAuthority: string;
+  caseNumber: string;
+  cryptoCurrencyOwnerData: string;
+  cryptoAssets: { cryptoAsset: string; amountOfCryptoAsset: string }[];
+};
 
-const INPUT_CANT_BE_EMPTY = "Pole nie może być puste";
+const INPUT_CANT_BE_EMPTY = 'Pole nie może być puste';
 
 function NewReport() {
-    const {
-        register,
-        control,
-        handleSubmit,
-        formState: {errors},
-    } = useForm<Inputs>({
-        defaultValues: {
-            cryptoAssets: [{cryptoAsset: '', amountOfCryptoAsset: ''}],
-        },
-    });
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      cryptoAssets: [{ cryptoAsset: '', amountOfCryptoAsset: '' }],
+    },
+  });
 
-    const {fields, append, remove} = useFieldArray({
-        control,
-        name: 'cryptoAssets',
-    });
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'cryptoAssets',
+  });
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-
-    return (
-        <Wrapper>
-            <header>
-                <Typography component="h1" variant="h5">
-                    Utwórz nowy raport
-                </Typography>
-            </header>
-            <Divider/>
-            <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{mt: 3}}>
-                <Grid container justifyContent="center" spacing={2}>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            error={!!errors.enforcementAuthority}
-                            helperText={errors.enforcementAuthority ? INPUT_CANT_BE_EMPTY : null}
-                            fullWidth
-                            id="enforcementAuthority"
-                            label="Nazwa organu egzekucyjnego"
-                            {...register("enforcementAuthority", {required: true})}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            error={!!errors.caseNumber}
-                            helperText={errors.caseNumber ? INPUT_CANT_BE_EMPTY : null}
-                            fullWidth
-                            id="caseNumber"
-                            label="Numer sprawy"
-                            {...register("caseNumber", {required: true})}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            error={!!errors.cryptoCurrencyOwnerData}
-                            helperText={errors.cryptoCurrencyOwnerData ? INPUT_CANT_BE_EMPTY : null}
-                            fullWidth
-                            id="cryptoCurrencyOwnerData"
-                            label="Dane identyfikujące właściciela kryptowaluty"
-                            {...register("cryptoCurrencyOwnerData", {required: true})}
-                        />
-                    </Grid>
-                    {fields.map((field, index) => (
-                        <Grid item xs={12} key={field.id} container spacing={2} alignItems = "center">
-                            <Grid item xs={fields.length === 1 ? 6 : 5}>
-                                <TextField
-                                    fullWidth
-                                    label="Nazwa Kryptoaktywa"
-                                    defaultValue={field.cryptoAsset}
-                                    {...register(`cryptoAssets.${index}.cryptoAsset`)}
-                                />
-                            </Grid>
-                            <Grid item xs={fields.length === 1 ? 6 : 5}>
-                                <TextField
-                                    fullWidth
-                                    label="Ilość kryptoaktywów"
-                                    defaultValue={field.amountOfCryptoAsset}
-                                    {...register(`cryptoAssets.${index}.amountOfCryptoAsset`)}
-                                />
-                            </Grid>
-                            {fields.length > 1 && (
-                            <Grid item xs={2}>
-                                <Button type="button" color="warning" variant="contained" onClick={() => remove(index)}>
-                                    Usuń
-                                </Button>
-                            </Grid>)}
-                        </Grid>
-                    ))}
-                    <Grid item xs={6}>
-                        <Button variant="contained" color="primary"
-                                onClick={() => append({cryptoAsset: '', amountOfCryptoAsset: ''})}>
-                            + Dodaj kryptoaktywo
-                        </Button>
-                    </Grid>
-                </Grid>
-                <Button
-                    type="submit"
-                    fullWidth
+  return (
+    <Wrapper>
+      <header>
+        <Typography component="h1" variant="h5">
+          Utwórz nowy raport
+        </Typography>
+      </header>
+      <Divider />
+      <Box
+        component="form"
+        noValidate
+        onSubmit={handleSubmit(onSubmit)}
+        sx={{ mt: 3 }}
+      >
+        <Grid container justifyContent="center" spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              error={!!errors.enforcementAuthority}
+              helperText={
+                errors.enforcementAuthority ? INPUT_CANT_BE_EMPTY : null
+              }
+              fullWidth
+              id="enforcementAuthority"
+              label="Nazwa organu egzekucyjnego"
+              {...register('enforcementAuthority', { required: true })}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              error={!!errors.caseNumber}
+              helperText={errors.caseNumber ? INPUT_CANT_BE_EMPTY : null}
+              fullWidth
+              id="caseNumber"
+              label="Numer sprawy"
+              {...register('caseNumber', { required: true })}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              error={!!errors.cryptoCurrencyOwnerData}
+              helperText={
+                errors.cryptoCurrencyOwnerData ? INPUT_CANT_BE_EMPTY : null
+              }
+              fullWidth
+              id="cryptoCurrencyOwnerData"
+              label="Dane identyfikujące właściciela kryptowaluty"
+              {...register('cryptoCurrencyOwnerData', { required: true })}
+            />
+          </Grid>
+          {fields.map((field, index) => (
+            <Grid
+              item
+              xs={12}
+              key={field.id}
+              container
+              spacing={2}
+              alignItems="center"
+            >
+              <Grid item xs={fields.length === 1 ? 6 : 5}>
+                <TextField
+                  fullWidth
+                  label="Nazwa Kryptoaktywa"
+                  defaultValue={field.cryptoAsset}
+                  {...register(`cryptoAssets.${index}.cryptoAsset`)}
+                />
+              </Grid>
+              <Grid item xs={fields.length === 1 ? 6 : 5}>
+                <TextField
+                  fullWidth
+                  label="Ilość kryptoaktywów"
+                  defaultValue={field.amountOfCryptoAsset}
+                  {...register(`cryptoAssets.${index}.amountOfCryptoAsset`)}
+                />
+              </Grid>
+              {fields.length > 1 && (
+                <Grid item xs={2}>
+                  <Button
+                    type="button"
+                    color="warning"
                     variant="contained"
-                    sx={{mt: 3, mb: 2}}
-                    style={{backgroundColor: 'green', color: 'white'}}
-                >
-                    Generuj Raport
-                </Button>
-            </Box>
-        </Wrapper>
-    )
+                    onClick={() => remove(index)}
+                  >
+                    Usuń
+                  </Button>
+                </Grid>
+              )}
+            </Grid>
+          ))}
+          <Grid item xs={6}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() =>
+                append({ cryptoAsset: '', amountOfCryptoAsset: '' })
+              }
+            >
+              + Dodaj kryptoaktywo
+            </Button>
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          style={{ backgroundColor: 'green', color: 'white' }}
+        >
+          Generuj Raport
+        </Button>
+      </Box>
+      <Button
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+        style={{ backgroundColor: 'green', color: 'white' }}
+      >
+        <Link to="/manual">Podaj dane ręcznie</Link>
+      </Button>
+    </Wrapper>
+  );
 }
 
 export default NewReport;
